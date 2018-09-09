e04bd94ee66d3f91e040614edaf25f2042ee4a25
ayabongaqwabi <ayabonga@projectcodex.co>
2018-09-09 11:17:02 +0200

    Convert Adventurers home page to South African illustraitions home

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,37 @@
 import Link from 'next/link';
+import Header from '../components/header';
+import Post from '../components/post';
 
 const home = () =>(
-    <div className='adventure'>
-        <div className='join-adventures'> 
-            <h1>The Adventurers Code </h1>
-            <p>Are you ready for a quest ?</p>
-            <Link href='/join'>
-                <button> Join </button>
-            </Link>
-        </div>
-        <div className='mural'>
-            <img src='/static/m.png'/>
-        </div>
-        <style jsx>{`
-            .adventure{
-                margin:200px 300px;
-                background:  #211041;
-                font-family: Tahoma;
-                padding: 50px;
-                color:#fff;
-                text-align:left;
-                box-shadow: 1px 1px 1px grey;
-                
-            }
-            .join-adventures{
-                margin:auto;
-                margin-right:260px;
-                width:350px;
-                display:inline-block;
-            }
-            .mural{
-                margin:auto;
-                text-align: center;
-                width:350px;
-                display:inline-block;
-                filter: saturate(8);
-            }
-            .mural img{
-                max-width:50%;
-            }
-            button{
-                padding: 10px;
-                background: #ed3990;
-                color: #fff;
-                border: 3px solid #ffeb3b00;
-                width: 120px;
-                border-radius: 5px;
-                font-weight: 600;
-            }
+    <div>
+        <Header title='South African illustrations' subtitle='as seen on facebook' />
+        <div className='adventure'>
+            <Post 
+                image='/static/artwar.jpg' 
+                title='Nkululeko Zulu' 
+                description="This was just meant to be a hair study in vector to see what I can achieve... I'm on Instagram too"
+                username='@artnwar' 
+            />
 
-            @keyframes grad{
-                0%{
-                   trasnform: scale(1) rotateZ(-1deg);
+            <Post 
+                image='/static/sibbambo.jpg' 
+                title='Tumi Sibambo' 
+                description="The kid"
+                username='@SibamboPotraits' 
+            />
+          
+            <style jsx>{`
+                .adventure{
+                    margin:0px 200px;
+                    font-family: Futura;
+                    padding-top: 10px;
+                    padding-left: 250px;
+                    color:#fff;
+                    text-align:left;
                 }
-                100%{
-                    transform: scale(1.02) rotateZ(1deg) rotate3d(1, 1, 1, 5deg);
-                    
                 }
-            }
-        `}</style>
+            `}</style>
+        </div>
     </div>
 );
 
