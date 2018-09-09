f8937608b230328ca32adc9222fc58262d07817b
ayabongaqwabi <ayabonga@projectcodex.co>
2018-09-09 23:03:01 +0200

    optiumize post component

--- components/feed.js
+++ components/feed.js
@@ -3,30 +3,21 @@ import { Post, MultiPost } from './post';
 const retroImages = ['/static/images/m1.jpg','/static/images/m2.jpg','/static/images/m3.jpg','/static/images/m4.jpg']
 
 const feed = props => {
-
+    const {posts} = props;
+    console.log(props)
     return (
         <div className='feed'>
-            <Post 
-                image='/static/images/artwar.jpg' 
-                title='Nkululeko Zulu' 
-                description="This was just meant to be a hair study in vector to see what I can achieve... I'm on Instagram too"
-                username='@artnwar' 
-            />
-
-            <Post 
-                image='/static/images/chris.jpg' 
-                title='Christopher Junior Masuabi' 
-                description="Christopher the chuckle bberg  #ChrisDesign"
-                username='@chris' 
-            />
-          
-            <MultiPost 
-                images={retroImages}
-                title='Ntsako Terence Maluleke' 
-                description="Limited Edition WAY WAY Iconic Collection prints"
-                username='@wayway_sa' 
-            />
-
+            {
+                posts.map( post => {
+                    if(post.type === 'single'){
+                        return <Post {...post} />
+                    }
+                    else{
+                        return <MultiPost {...post} />
+                    }
+                    
+                })
+            }
             <style jsx>{`
                 .feed{
                     margin:0px 200px;
