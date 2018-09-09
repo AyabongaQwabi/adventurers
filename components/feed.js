b3c9361113fecca09225b4576acede92b66a17a2
ayabongaqwabi <ayabonga@projectcodex.co>
2018-09-09 21:50:39 +0200

    Update image urls

--- components/feed.js
+++ components/feed.js
@@ -1,20 +1,20 @@
 
 import { Post, MultiPost } from './post';
-const retroImages = ['/static/m1.jpg','/static/m2.jpg','/static/m3.jpg','/static/m4.jpg']
+const retroImages = ['/static/images/m1.jpg','/static/images/m2.jpg','/static/images/m3.jpg','/static/images/m4.jpg']
 
 const feed = props => {
 
     return (
         <div className='feed'>
             <Post 
-                image='/static/artwar.jpg' 
+                image='/static/images/artwar.jpg' 
                 title='Nkululeko Zulu' 
                 description="This was just meant to be a hair study in vector to see what I can achieve... I'm on Instagram too"
                 username='@artnwar' 
             />
 
             <Post 
-                image='/static/chris.jpg' 
+                image='/static/images/chris.jpg' 
                 title='Christopher Junior Masuabi' 
                 description="Christopher the chuckle bberg  #ChrisDesign"
                 username='@chris' 
