8393ad3debc4ccc23c2a943f989c53619278d8ec
ayabongaqwabi <ayabonga@projectcodex.co>
2018-09-09 23:03:31 +0200

    Update :  Add neew tag

--- components/filter.js
+++ components/filter.js
@@ -1,19 +1,19 @@
 // ./pages/_document.js
 import { Container, Row, Col } from 'reactstrap';
 
-const getTags = () => ['mural','black-and-white','retro'];
+const getTags = () => ['mural','black-and-white','retro', 'all'];
 const getUsers = () => ['@wayway','@chris','@artnwar'];
-
+const retroImages = ['/static/images/m1.jpg','/static/images/m2.jpg','/static/images/m3.jpg','/static/images/m4.jpg']
 const filter = props => {
     const { onFilter } = props
     return (
         <div className='filter'>
             <Row>
-                <Col md='2'><p>Filter by :</p></Col>
-                <Col md='10'>
+                <Col md='6'><p></p></Col>
+                <Col md='6'>
                     {getTags().map(tag => {
                         return(
-                            <a href='#' key={tag} onClick={onFilter(tag)}> {tag} </a>
+                            <a href='#' key={tag} onClick={onFilter(tag)} className='tag'> {tag} </a>
                         )
                     })}
                 </Col>
