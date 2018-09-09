c832ecc9bdb6e7375aad54ec75909a205ed70d0a
ayabongaqwabi <ayabonga@projectcodex.co>
2018-09-09 21:50:12 +0200

    Use bbootstrap sitewide

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
-import Link from 'next/link';
+
 import Header from '../components/header';
+import Filter from '../components/filter';
 import Feed from '../components/feed';
+
+const setFilter = (item) => () =>{
+
+}
 
 const home = () =>(
     <div>
         <Header title='South African illustrations' subtitle='as seen on facebook' />
+        <Filter onFilter={setFilter} />
         <Feed />
+        <style jsx>{`@import '/static/css/bootstrap/bootstrap.min.css'`}</style>
     </div>
 );
 
