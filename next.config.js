b2ea5c0f035b45649e5b435082bad4e8ac930b98
ayabongaqwabi <ayabonga@projectcodex.co>
2018-09-09 23:35:38 +0200

    Update config for deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
   cssModules: true,
+  assetPrefix: !debug ? '/adveturers/' : '',
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: "[local]___[hash:base64:5]",
