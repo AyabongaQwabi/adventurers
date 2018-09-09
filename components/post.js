36146c132e66053aaa72b7914f1b0aa7ee711fcb
ayabongaqwabi <ayabonga@projectcodex.co>
2018-09-09 11:16:33 +0200

    Feat: Add post component

---
+++ components/post.js
@@ -0,0 +1,54 @@
+const post = props => {
+    const { image, username, title, description  } = props
+    return (
+        <div className='post'>
+            <div className='post-image'>
+                <img src={image} />
+            </div>
+            <div className='post-user'>
+                <h4>{title}</h4>
+                <p><small>{description}</small></p>
+                <h5>{username}</h5>
+            </div>
+            <style jsx>{`
+            .post{
+                padding: 20px;
+                text-align: left;
+                font-family: Futura;
+            }
+            .post .post-image{
+                height: 400px;
+                width: 400px;
+                display: inline-block;
+            }
+            .post .post-image img{
+                max-width: 100%;
+            }
+            .post .post-user{
+                height: 100px;
+                width: 400px;
+                display: inline-block;
+                vertical-align:top;
+                margin-top: 10%;
+                padding-left: 10%;
+                color: #000;
+            }
+            .post .post-user h1{
+                margin-top: 0px;
+                margin-bottom: 5px;
+            }
+            .post .post-user h6{
+                margin-left: 1%;
+                margin: 0px;
+            }
+            .post .post-user h6:before{
+                content:'-';
+                padding-left:10px;
+                padding-right:10px;
+            }
+        `}</style>
+        </div>
+    )
+}
+
+export default post;
